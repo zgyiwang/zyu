853cbcda2d2cb6657515915aaf3026cfc176e535
baizhuoyu <852030019@qq.com>
2020-11-22 15:55:49 +0800

    提交修改

--- zyu-webserver/src/main/java/com/zyu/webserver/controller/webRequestController.java
+++ zyu-webserver/src/main/java/com/zyu/webserver/controller/webRequestController.java
@@ -2,7 +2,6 @@ package com.zyu.webserver.controller;
 
 import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class webRequestController {
     @RequestMapping
     public void ak1() {
-        String a = "";
+        String a = "123";
         JSON.parse(a);
     }
 }
